51223bde0cf99d9e70b1c6c32176d18e433bad3e
sobhit-raghav <shobit.raghav@solytics-partners.com>
2025-10-03 03:13:36 +0530

    using morgan for structured logging

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 
 import influencerRoutes from './routes/influencerRoutes.js';
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send('API is running successfully...');
